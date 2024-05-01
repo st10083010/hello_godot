ad94b288e845c0966cb57946504cd2ca424497ad
奕翔 錢 <st10083010@gmail.com>
2024-05-01 18:12:09 +0800

    export

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,6 +1,12 @@
 extends Area2D
 
+# 使用unique name引用而不是path
+@onready var game_manager = %Game_Manager
+@onready var animation_player = $AnimationPlayer
 
 func _on_body_entered(body):
+	game_manager.add_score()
 	print("+1 coin")
-	queue_free()
+	
+	# 透過anmation player處理部分音訊及釋放資源
+	animation_player.play("pickup")
